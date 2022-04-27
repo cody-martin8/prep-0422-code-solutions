47102bfc7d524cd86f26a8d2e3f2f8aec2bbf6db
Cody Martin <12martincody@gmail.com>
2022-04-27 22:27:52 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+// for (let a = 0; a < 10; a++) {
+//   console.log(a);
+// }
+
+// for (let b = 0; b < 20; b = b + 2) {
+//   console.log(b);
+// }
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+}
